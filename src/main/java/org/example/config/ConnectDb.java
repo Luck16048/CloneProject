1c08ffce702624a070a00932f91a0484192c17ed
Luck16048 <karinaparhomenko2007@gmail.com>
2026-05-04 18:25:31 +0200

    Add MySQL and Redis connection

---
+++ src/main/java/org/example/config/ConnectDb.java
@@ -0,0 +1,24 @@
+package org.example.config;
+
+import org.jooq.DSLContext;
+import org.jooq.SQLDialect;
+import org.jooq.impl.DSL;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.DriverManager;
+
+public class ConnectDb {
+    private static final String URL = System.getenv("URL");
+    private static final String USERNAME = System.getenv("USERNAME");
+    private static final String PASSWORD =  System.getenv("PASSWORD");
+
+    public static Connection getConnection() throws SQLException{
+        return DriverManager.getConnection(URL, USERNAME, PASSWORD);
+    }
+
+    public static DSLContext getDSL() throws SQLException{
+        Connection conn = getConnection();
+        return DSL.using(conn, SQLDialect.MYSQL);
+    }
+}
